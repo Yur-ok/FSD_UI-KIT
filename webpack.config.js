8139ad9b0961713242f2af525996dd7f0c473107
Yur-ok <Materik-r@yandex.ru>
2018-10-23 14:30:17 +0700

    added config for webpack & postcss

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path');
+
+// Для работы с HTML
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Для создания css файла
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// Для отчистки дериктории назначения перед созданием сборки
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+//
+const WebpackMd5Hash = require('webpack-md5-hash');
+
+//
+const webpack = require('webpack');
+
+
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        main: './index.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: '[name].bandle.[chunkhash].js',
+    },
+
+    optimization: {
+        noEmitOnErrors: true,
+
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                exclude: /(node_modules|.git)/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            },
+            {
+                test: /\.styl$/,
+                exclude: /(node_modules|.git)/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'stylus-loader?resolve url'
+                ]
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf)$/,
+                exclude: /(node_modules|.git)/,
+                loader: 'file-loader',
+                options: {
+                    name: "./fonts/[name].[ext]",
+                }
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                exclude: /(node_modules|.git)/,
+                loader: 'file-loader',
+                options: {
+                    name: "./img/[name].[ext]",
+                }
+            }
+        ]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'docs'),
+        compress: true,
+        overlay: true,
+        open: true
+    },
+
+    plugins: [
+        new CleanWebpackPlugin('docs', {}),
+        new MiniCssExtractPlugin({
+            filename: 'style.[hash].css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            hash: true,
+            cache: true,
+            template: 'index.pug',
+            filename: 'index.html'
+        }),
+        new WebpackMd5Hash(),
+    ]
+};
