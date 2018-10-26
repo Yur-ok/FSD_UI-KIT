4563dd4ebb5579f172051a7964cdd9d8cba99f3a
Yur-ok <Materik-r@yandex.ru>
2018-10-26 16:11:43 +0700

    done with buttons

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 import './style.styl';
+import 'jquery';
+import 'jquery-ui';
+
+//ripple effect for buttons
+import { defaultOptions } from 'ripplet.js/es/ripplet-declarative';
+defaultOptions.color = 'rgba(230, 220, 255, .3)';
+
+
+function requireAll(requireContext) {
+    return requireContext.keys().map(requireContext);
+}
+// requires and returns all modules that match
+var modules = requireAll(require.context("./src/", true, /^\.\/.*\.js$/));
+// is an array containing all the matching modules
+
+
+
+//jQuery test
+(function () {
+
+    $(document).ready(function() {
+        console.log("It works!");
+    });
+
+})();
