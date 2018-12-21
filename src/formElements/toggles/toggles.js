f6ab97a1a76110a61b6fad5e8e228c77f34206a7
Yur-ok <Materik-r@yandex.ru>
2018-12-21 16:49:17 +0700

    working on responsive behavior

--- src/formElements/toggles/toggles.js
+++
@@ -1,5 +0,0 @@
-//https://www.jqueryscript.net/form/ON-OFF-Toggle-Switches-Switcher.html
-
-$(document).ready(function () {
-    $.switcher('.form-check-input');
-});
--- src/sliders/sliders.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('#leftSl').ionRangeSlider();
-    $('#rightSl').ionRangeSlider();
-
-});
